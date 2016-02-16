401f3232b6f6a93bcacbe1d54c64b45abc7cf78c
Blair Peterson and Joshua Gustafson <blairchalmers@gmail.com>
2016-02-16 08:40:27 -0800

    add first test

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,33 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest { //extends FluentTest
+  // public WebDriver webDriver = new HtmlUnitDriver();
+  // public WebDriver getDefaultDriver() {
+  //     return webDriver;
+  // }
+  //
+  // @ClassRule
+  // public static ServerRule server = new ServerRule();
+  //
+  // @Test
+  // public void rootTest() {
+  //     goTo("http://localhost:4567/");
+  //     assertThat(pageSource()).contains("Leap year detector");
+  // }
+
+  @Test
+  public void Task_instantiatesCorrectly_true() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(true, myTask instanceof Task);
+  }
+}
