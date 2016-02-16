a01741e0bb4c65f727ca2a281b990e28207c4ee4
Blair Peterson and Joshua Gustafson <silversevenfox@yahoo.com>
2016-02-16 08:44:50 -0800

    add second test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -30,4 +30,10 @@ public class AppTest { //extends FluentTest
     Task myTask = new Task("Mow the lawn");
     assertEquals(true, myTask instanceof Task);
   }
+
+  @Test
+  public void task_instantiatesWithDescription_true() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals("Mow the lawn", myTask.getDescription());
+  }
 }
